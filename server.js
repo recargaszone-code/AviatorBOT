52386a2b82fb110c659a580fa3e13bf4d4f29751
recargaszone-code <recargaszone@gmail.com>
2026-02-19 23:44:46 -0800

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const TELEGRAM_TOKEN = "8583470384:AAF0poQRbfGkmGy7cA604C4b_-MhYj-V7XM";
 const CHAT_ID = "7427648935";
 const TELEFONE = "863584494";
 const SENHA = "0000000000";
-const URL_AVIATOR = 'https://888bets.co.mz/pt/games/detail/casino/normal/7787';
+const URL_AVIATOR = 'https://m.888bets.co.mz/pt/games/detail/casino/normal/7787';
 
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: false });
 
@@ -184,6 +184,7 @@ process.on('SIGTERM', async () => {
   if (browser) await browser.close();
   process.exit(0);
 });
+
 
 
 
